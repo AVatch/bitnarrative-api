a8af36333db22a82c57d2c069e4553690960ff1f
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-03-20 14:32:01 -0400

    bits are no longer rendered in content response because there are too many

--- bit_narrative_api/content/serializers.py
+++ bit_narrative_api/content/serializers.py
@@ -1,19 +1,14 @@
 from rest_framework import serializers
 
 from content.models import Content
-from bits.serializers import BitSerializer
 
 
 class ContentSerializer(serializers.HyperlinkedModelSerializer):
-    # bits = serializers.HyperlinkedRelatedField(many=True,
-    #                                            view_name='bit-detail',
-    #                                            read_only=True)
-    bits = BitSerializer(many=True, read_only=True)
 
     class Meta:
         model = Content
         fields = ('id', 'domain', 'url',
-                  'title', 'excerpt', 'content', 'bits',
+                  'title', 'excerpt', 'content',
                   'lead_image_url', 'date_published',
                   'word_count', 'view_count', 'share_count',
                   'created_at', )
--- bit_narrative_api/content/views.py
+++ bit_narrative_api/content/views.py
@@ -26,24 +26,6 @@ class ContentList(generics.ListAPIView):
                               authentication.TokenAuthentication)
     permission_classes = (permissions.IsAuthenticated,)
 
-    # Override create method to parse the content
-    # -- ISSUE: There is an issue with saving 'date_published'
-    def perform_create(self, serializer):
-        if not self.request.data[u'domain']:
-            # only sending the url, so parse the content
-            parsed = parse_content(self.request.data[u'url'])
-            serializer.save(domain=parsed['domain'],
-                            url=parsed['url'],
-                            title=parsed['title'],
-                            excerpt=parsed['excerpt'],
-                            content=parsed['content'],
-                            lead_image_url=parsed['lead_image_url'],
-                            word_count=parsed['word_count'],
-                            view_count=1,
-                            share_count=0, )
-        else:
-            serializer.save()
-
 
 class ContentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
